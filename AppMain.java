62dd19d9761d1fcb1224de5736531e349c5a8293
AdrianSchulze <adrian@sei-sicherer.de>
2022-11-22 22:01:37 +0100

    System out shows Array numbers

---
+++ AppMain.java
@@ -0,0 +1,13 @@
+public class AppMain {
+    public static void main(String [] args) {
+
+
+        int[] numbers = new int[]{1, 2, 4, 6, 78};
+        int[] number2 = {1,2,3,4,5,6,7,8};
+        System.out.println(numbers[0] + numbers[2]);
+        System.out.println(number2[1]);
+        System.out.println(number2.length);
+
+
+    }
+}
